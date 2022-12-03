cc89cd1206ea61a05674b951013f3bc2ebe85a44
Sharma262 <107828861+Sharma262@users.noreply.github.com>
2022-12-04 05:20:00 +0530

    Add files via upload

---
+++ RestApi.js
@@ -0,0 +1,242 @@
+let object = [
+        {
+            "Id": 1,
+            "Name": "Himanshu Belsare",
+            "Email": "himanshu.belsare@legalpay.in",
+            "Phone": "7066262873",
+            "Comission": 2.0,
+            "kyc": "Success",
+            "Pending_actions": "Kyc Done,E-sign pending",
+            "Investment": 150000,
+            "ref": 6,
+            "Agreements": "Unsigned",
+            "spv_details": [
+                {
+                    "spvName": "Interim Finance & Real Estate SPV I",
+                    "invested_money": "50000",
+                    "date_of_investment": "2022-07-12"
+                },
+                {
+                    "spvName": "LegalPay & Arbitration Focused SPV I",
+                    "invested_money": "100000",
+                    "date_of_investment": "2022-07-07"
+                }
+            ]
+        },
+        {
+            "Id": 2,
+            "Name": "uttam kumar",
+            "Email": "uttam.kumar@legalpay.in",
+            "Phone": "6299268964",
+            "Comission": 0.5,
+            "kyc": "Pending",
+            "Pending_actions": "Kyc Pending,E-sign Done",
+            "Investment": 100000,
+            "ref": 11,
+            "Agreements": "Signed",
+            "spv_details": [
+                {
+                    "spvName": "LegalPay & Smaller-Ticket Commercial Disputes SPV I",
+                    "invested_money": "100000",
+                    "date_of_investment": "2022-07-26"
+                }
+            ]
+        },
+        {
+            "Id": 3,
+            "Name": "shivendra singh",
+            "Email": "shivendra.singh@legalpay.in",
+            "Phone": "8000495359",
+            "Comission": 4.6,
+            "kyc": "Success",
+            "Pending_actions": "Kyc Done,E-sign pending",
+            "Investment": 200000,
+            "ref": 22,
+            "Agreements": "Unsigned",
+            "spv_details": [
+                {
+                    "spvName": "Interim Finance & Healthcare Focused SPV I",
+                    "invested_money": "200000",
+                    "date_of_investment": "2022-07-26"
+                }
+            ]
+        },
+        {
+            "Id": 4,
+            "Name": "amit kumar",
+            "Email": "amit.kumar@legalpay.in",
+            "Phone": "9891364199",
+            "Comission": 3.5,
+            "kyc": "Success",
+            "Pending_actions": "Kyc Done,E-sign pending",
+            "Investment": 0,
+            "ref": 0,
+            "Agreements": "Unsigned",
+            "spv_details": []
+        },
+        {
+            "Id": 5,
+            "Name": "himansu kesari",
+            "Email": "himansu@legalpay.in",
+            "Phone": "9711918082",
+            "Comission": 1.5,
+            "kyc": "Pending",
+            "Pending_actions": "Kyc Pending,E-sign Done",
+            "Investment": 75000,
+            "ref": 0,
+            "Agreements": "Signed",
+            "spv_details": [
+                {
+                    "spvName": "LegalPay & Arbitration Focused SPV I",
+                    "invested_money": "75000",
+                    "date_of_investment": "2022-07-26"
+                }
+            ]
+        },
+        {
+            "Id": 6,
+            "Name": "Dummy Data",
+            "Email": "dummy@gmail.com",
+            "Phone": "9179587904",
+            "Comission": 1.5,
+            "kyc": "Success",
+            "Pending_actions": "Kyc Done,E-sign Done",
+            "Investment": 2000,
+            "ref": 0,
+            "Agreements": "Signed",
+            "spv_details": [
+                {
+                    "spvName": "Real Estate (Healthcare + Manufacturing) SPV I",
+                    "invested_money": "2000",
+                    "date_of_investment": "2022-07-23"
+                }
+            ]
+        },
+        {
+            "Id": 7,
+            "Name": "Ayush kolte",
+            "Email": "dummy1@gmail.com",
+            "Phone": "9179587905",
+            "Comission": 1.0,
+            "kyc": "Success",
+            "Pending_actions": "Kyc Done,E-sign pending",
+            "Investment": 0,
+            "ref": 0,
+            "Agreements": "Unsigned",
+            "spv_details": []
+        },
+        {
+            "Id": 8,
+            "Name": "himansu Belsare",
+            "Email": "dummy2@gmail.com",
+            "Phone": "9179587906",
+            "Comission": 4.0,
+            "kyc": "Success",
+            "Pending_actions": "Kyc Done,E-sign pending",
+            "Investment": 6050000,
+            "ref": 0,
+            "Agreements": "Unsigned",
+            "spv_details": [
+                {
+                    "spvName": "Real Estate (Healthcare + Manufacturing) SPV I",
+                    "invested_money": "50000",
+                    "date_of_investment": "2022-07-15"
+                },
+                {
+                    "spvName": "Interim Finance & Production Studio (Real Estate Backed) SPV I",
+                    "invested_money": "6000000",
+                    "date_of_investment": "2022-07-22"
+                }
+            ]
+        },
+        {
+            "Id": 9,
+            "Name": "Abbas Khan",
+            "Email": "dummy3@gmail.com",
+            "Phone": "9179587907",
+            "Comission": 2.5,
+            "kyc": "Success",
+            "Pending_actions": "Kyc Done,E-sign pending",
+            "Investment": 0,
+            "ref": 0,
+            "Agreements": "Unsigned",
+            "spv_details": []
+        },
+        {
+            "Id": 10,
+            "Name": "Uttam Kumar",
+            "Email": "uttam1046@gmail.com",
+            "Phone": "9162884890",
+            "Comission": null,
+            "kyc": "Pending",
+            "Pending_actions": "Kyc Pending,E-sign pending",
+            "Investment": 10000,
+            "ref": 1,
+            "Agreements": "Unsigned",
+            "spv_details": [
+                {
+                    "spvName": "Interim Finance & Estate Developer SPV I",
+                    "invested_money": "10000",
+                    "date_of_investment": "2022-07-22"
+                }
+            ]
+        },
+        {
+            "Id": 11,
+            "Name": "dummy1 Kumar",
+            "Email": "dummy123@gmail.com",
+            "Phone": "8540001201",
+            "Comission": null,
+            "kyc": "Pending",
+            "Pending_actions": "Kyc Pending,E-sign pending",
+            "Investment": 0,
+            "ref": 0,
+            "Agreements": "Unsigned",
+            "spv_details": []
+        },
+        {
+            "Id": 12,
+            "Name": "dummy2 kumar",
+            "Email": "dummu2132@gmail.com",
+            "Phone": "9182626737",
+            "Comission": null,
+            "kyc": "Pending",
+            "Pending_actions": "Kyc Pending,E-sign pending",
+            "Investment": 0,
+            "ref": 0,
+            "Agreements": "Unsigned",
+            "spv_details": []
+        },
+        {
+            "Id": 13,
+            "Name": "dummy singh",
+            "Email": "dummysingh@legalpay.in",
+            "Phone": "8871542405",
+            "Comission": null,
+            "kyc": "Pending",
+            "Pending_actions": "Kyc Pending,E-sign pending",
+            "Investment": 0,
+            "ref": 0,
+            "Agreements": "Unsigned",
+            "spv_details": []
+        },
+        {
+            "Id": 14,
+            "Name": "shivendra rajput",
+            "Email": "shivendra.singh@legalpay.in",
+            "Phone": "8000495359",
+            "Comission": 4,
+            "kyc": "pending",
+            "Pending_actions": "Kyc Done,E-sign pending",
+            "Investment": 20000,
+            "ref": 12,
+            "Agreements": "Signed",
+            "spv_details": [
+                {
+                    "spvName": "Interim Finance & Healthcare Focused SPV I",
+                    "invested_money": "200000",
+                    "date_of_investment": "2022-07-26"
+                }
+            ]
+        }
+]
